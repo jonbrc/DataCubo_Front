97b4f9484aa86f9c15f242753c7ea1d3703a62ad
jonbrc <jbr.carvalho18@gmail.com>
2025-08-27 20:03:00 -0300

    Criação do Projeto angular

---
+++ DataCubo/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FileUploadComponent,
+    DataTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
